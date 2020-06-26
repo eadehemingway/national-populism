9950217ab7b3620029fe41f87eb8fa3556878c10
eadehemingway <eade.hemingway@gmail.com>
2020-06-26 16:47:37 +0200

    adjust

---
+++ src/components/MapNationalPopulism.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect, useRef } from "react"
+import styled from "styled-components"
+import * as d3 from "d3"
+import data from "../data/mapEuropeWithElectionData"
+
+export default function NationalPopulism() {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    mountD3Map()
+  }, [])
+
+  useEffect(() => {
+    if (index >= 39) return
+    let id = setTimeout(() => {
+      setIndex(index + 1)
+    }, 300)
+    return () => clearTimeout(id)
+  })
+
+  useEffect(() => {
+    reColourMap(index)
+    updateTooltip()
+  }, [index])
+
+  function reColourMap(index) {
+    d3.selectAll("path").attr("fill", country => {
+      const value =
+        country.properties.electionData &&
+        country.properties.electionData[index].value
+      const noDataForCountry = !value
+      const noDataForYear = value === "x"
+      const transparent = noDataForCountry || noDataForYear
+      if (transparent) return "transparent"
+      const shadeOfGreen = d3.interpolateReds(value / 100)
+      return shadeOfGreen
+    })
+  }
+
+  function mountD3Map() {
+    const projection = d3
+      .geoMercator()
+      .scale(1500 / Math.PI / 2)
+      .translate([580, 450])
+
+    const svg = d3.select("#map").attr("width", 1500).attr("height", 900)
+
+    const countryGroups = svg
+      .selectAll(".country-groups")
+      .data(data.features)
+      .enter()
+      .append("g")
+      .attr("class", "country-groups")
+
+    countryGroups
+      .append("path")
+      .attr("d", d3.geoPath(projection))
+      .attr("stroke-width", "1px")
+      .attr("stroke-opacity", 0.1)
+      .attr("opacity", 0.7)
+      .attr("stroke", "steelblue")
+      .attr("transform", `scale(2) translate(-300, 0)`)
+
+    const tooltipGroups = svg
+      .selectAll(".tooltip-groups")
+      .data(data.features)
+      .enter()
+      .append("g")
+      .attr("class", "tooltip-groups")
+      .attr("class", d => `tooltip-${d.properties.name_long}`)
+      .style("visibility", "hidden")
+
+    tooltipGroups
+      .append("rect")
+      .attr("class", "tooltip-rect")
+      .attr("width", 200)
+      .attr("height", 60)
+      .attr("fill", "white")
+
+    tooltipGroups
+      .append("text")
+      .attr("class", "tooltip-text")
+      .attr("x", 10)
+      .attr("y", 20)
+      .attr("font-family", "Major Mono")
+  }
+
+  function updateTooltip() {
+    const tooltipPadding = 10
+    const countryGroups = d3.selectAll(".country-groups")
+
+    d3.selectAll(".tooltip-text").text(d => {
+      if (d.properties.electionData) {
+        return `${d.properties.name_long}: ${d.properties.electionData[index].value}`
+      } else {
+        return `${d.properties.name_long}`
+      }
+    })
+
+    countryGroups
+      .on("mouseover", function (d) {
+        d3.select(`.tooltip-${d.properties.name_long}`)
+          .style("visibility", "visible")
+          .attr(
+            "transform",
+            `translate(${d3.event.offsetX + tooltipPadding},${
+              d3.event.offsetY
+            })`
+          )
+      })
+      .on("mousemove", d => {
+        d3.select(`.tooltip-${d.properties.name_long}`).attr(
+          "transform",
+          `translate(${d3.event.offsetX + tooltipPadding},${d3.event.offsetY})`
+        )
+      })
+      .on("mouseout", d =>
+        d3
+          .select(`.tooltip-${d.properties.name_long}`)
+          .style("visibility", "hidden")
+      )
+  }
+  return (
+    <MapWrapper>
+      <Year>{1980 + index}</Year>
+      <svg id="map" />
+    </MapWrapper>
+  )
+}
+
+const MapWrapper = styled.div`
+  height: 800px;
+  overflow: hidden;
+  padding: 10px;
+  position: absolute;
+  top: 100px;
+`
+const Year = styled.h1`
+  font-family: Major Mono;
+  position: absolute;
+  right: 50px;
+  top: 450px;
+  font-size: 100px;
+  color: grey;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,100 +1,26 @@
-import React, { useState } from "react"
-
+import React from "react"
 import styled from "styled-components"
-import { useEffect } from "react"
-import { Line } from "../components/Line"
+import NationalPopulism from "../components/MapNationalPopulism"
 import "../index.css"
-
-const IndexPage = () => {
-  const [lines, setLines] = useState()
-  const [linesArr, setLinesArr] = useState([])
-
-  function handleInputChange(e) {
-    const numLines = Number(e.target.value)
-
-    setLines(numLines)
-  }
-  useEffect(() => {
-    if (!lines) return
-    const arr = new Array(lines).fill("")
-
-    setLinesArr(arr)
-  }, [lines])
-
-  const copyToClipboard = () => {
-    const el = document.createElement("textarea")
-    const str = linesArr.join("\r\n")
-    el.value = str
-    document.body.appendChild(el)
-    el.select()
-    document.execCommand("copy")
-    document.body.removeChild(el)
-  }
-
+export default function ArticleTwo() {
   return (
     <Container>
-      <Header>
-        <PStyled>lines:</PStyled>
-        <Input
-          placeholder={0}
-          type="text"
-          onChange={handleInputChange}
-          defaultValue={lines}
-        />
-        <StyledBtn onClick={copyToClipboard}>Copy</StyledBtn>
-      </Header>
-      {linesArr.map((l, i) => {
-        return (
-          <Line
-            key={i}
-            index={i}
-            lineNumber={i + 1}
-            setLinesArr={setLinesArr}
-            linesArr={linesArr}
-          />
-        )
-      })}
+      <ArticleTitle>Rise of National Populism in Europe</ArticleTitle>
+      <NationalPopulism />
     </Container>
   )
 }
 
+const ArticleTitle = styled.h1`
+  width: 20%;
+  margin-top: 20px;
+  margin-left: 10px;
+  font-size: 30px;
+  color: #ff5c00;
+  text-transform: uppercase;
+  line-height: 42px;
+  font-family: Major Mono;
+`
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  position: relative;
 `
-
-const PStyled = styled.p`
-  font-family: Major Mono;
-  margin-right: 30px;
-`
-const Header = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-bottom: 60px;
-  margin-top: 100px;
-`
-const Input = styled.input`
-  width: 60px;
-  height: 30px;
-  border: 1px solid coral;
-  text-align: center;
-  font-family: Major Mono;
-  outline: none;
-`
-
-const StyledBtn = styled.button`
-  background: coral;
-  color: white;
-  border: 1px solid coral;
-  margin-left: 20px;
-  height: 30px;
-  font-family: Major Mono;
-  font-size: 14px;
-  cursor: pointer;
-  outline: none;
-  border-radius: 2px;
-  right: 100px;
-`
-
-export default IndexPage
